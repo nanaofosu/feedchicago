654c5c94ee024f82012553b4efa112cded3dea7d
Nana Ofosu Budu <nanaofosu@uchicago.edu>
2018-12-04 22:55:17 -0600

    Got the homeview controller working

--- src/app/assets/StringAssets.java
+++ src/app/assets/StringAssets.java
@@ -2,4 +2,5 @@ package app.assets;
 
 public interface StringAssets {
     public static final String USERNAME_EMPTY = "Username is empty";
+
 }
--- src/app/controller/HomeController.java
+++ src/app/controller/HomeController.java
@@ -1,24 +1,81 @@
 package app.controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HomeController {
 
     @FXML
-    private Button buttonAddSkill;
+    private Button addSkillButton;
 
     @FXML
-    private Button buttonAddVolunteer;
+    private Button addVolunteerButton;
 
     @FXML
-    private Button buttonViewSkills;
+    private Button viewSkillsButton;
 
     @FXML
-    private Button buttonViewVolunteers;
+    private Button viewVolunteersButton;
 
     @FXML
-    private Button buttonLogout;
+    private Button logoutButton;
+
+    private String ADD_SKILL_SCENE = "/app/view/addSkillsView.fxml";
+    private String ADD_VOLUNTEER_SCENE = "/app/view/addVolunteerView.fxml";
+    private String LOGOUT_SCENE = "/app/view/loginView.fxml";
+
+    @FXML
+    public void initialize() {
+
+        addSkillButton.setOnAction(event -> {
+            addSkillButton.getScene().getWindow().hide();
+            homerouter(ADD_SKILL_SCENE);
+        });
+
+        addVolunteerButton.setOnAction(event -> {
+            addSkillButton.getScene().getWindow().hide();
+            homerouter(ADD_VOLUNTEER_SCENE);
+        });
+
+        viewSkillsButton.setOnAction(event -> {
+            addSkillButton.getScene().getWindow().hide();
+            homerouter(ADD_SKILL_SCENE);
+        });
+
+        viewVolunteersButton.setOnAction(event -> {
+            addSkillButton.getScene().getWindow().hide();
+            homerouter(ADD_SKILL_SCENE);
+        });
+
+        logoutButton.setOnAction(event -> {
+            addSkillButton.getScene().getWindow().hide();
+            homerouter(LOGOUT_SCENE);
+        });
+
+    }
+
+    public void homerouter(String path) {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource(path));
+        try {
+            loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Parent root  = loader.getRoot();
+        Stage stage = new Stage();
+        stage.setScene(new Scene(root));
+        stage.showAndWait();
+    }
+
+
 
 
 }

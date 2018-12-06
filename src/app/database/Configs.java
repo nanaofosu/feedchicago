be9137f14334dc70a2ed29c631808625f0fb8580
Nana Ofosu Budu <nanaofosu@uchicago.edu>
2018-12-06 16:14:51 -0600

    Updated the password for the database

--- src/app/database/Configs.java
+++ src/app/database/Configs.java
@@ -1,9 +1,14 @@
 package app.database;
 
 public class Configs {
-    protected String dbHost = "localhost";
-    protected String dbPort = "33067";
-    protected String dbUser = "root";
-    protected String dbPass = "";
-    protected String dbName = "feed";
+//    protected String dbHost = "localhost";
+//    protected String dbPort = "33067";
+//    protected String dbUser = "root";
+//    protected String dbPass = "";
+//    protected String dbName = "feed";
+    protected String dbHost = "cs.neiu.edu";
+    protected String dbPort = "3306";
+    protected String dbUser = "18f315_Nana";
+    protected String dbPass = "18f315_Nana";
+    protected String dbName = "18f315_Nana";
 }
